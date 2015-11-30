fb01201881076d96ecbc3697436c0dbc9f634735
chemzqm <chemzqm@gmail.com>
2015-11-30 19:43:33 +0800

    Update documentation

---
+++ index.js
@@ -0,0 +1,16 @@
+var SwipeIt = require('..')
+var tap = require('tap-event')
+
+var template = '<div class="remove">删除</div>'
+var list = document.getElementById('list')
+
+var swipe = SwipeIt(template)
+swipe.bind(list, 'li')
+swipe.on('start', function (el) {
+})
+swipe.on('end', function (el) {
+})
+swipe.delegate('touchstart', '.remove', tap(function (e, el) {
+  el.parentNode.removeChild(el)
+  swipe.removeHolder()
+}))
