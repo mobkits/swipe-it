8fa4eb9705924cefa1a22e11717ec9bc841f0dd1
chemzqm <chemzqm@gmail.com>
2015-12-01 20:58:24 +0800

    improve example

--- example/index.js
+++ example/index.js
@@ -1,24 +1,41 @@
 if ('ontouchend' in window) {
   document.querySelector('#list .alert').style.display = 'none'
 }
+var detect = require('prop-detect')
+var transform = detect.transform
 var SwipeIt = require('..')
 var tap = require('tap-event')
 var Sortable = require('sweet-sortable')
 
-var template = '<div class="remove">删除</div>'
+!(function () {
+  function hide(el) {
+    el.style.display = 'none'
+  }
+  function show(el) {
+    el.style.display = 'block'
+  }
+  var template = '<div class="remove">删除</div>'
+  var list = document.getElementById('list')
+  // before swipe-it
+  var sortable = new Sortable(list)
+  sortable.handle('.handler')
+  sortable.bind('li')
+  //sortable.ignore('.swipe-dragging')
 
-var list = document.getElementById('list')
-var swipe = SwipeIt(template)
-swipe.bind(list, 'li')
-swipe.delegate('touchstart', '.remove', tap(function () {
-  swipe.remove()
-}))
+  var swipe = SwipeIt(template)
+  swipe.bind(list, 'li')
+  swipe.on('start', function (el) {
+    hide(el.querySelector('.handler'))
+  })
+  swipe.on('end', function (el) {
+    show(el.querySelector('.handler'))
+  })
+  swipe.delegate('touchstart', '.remove', tap(function () {
+    swipe.clear()
+  }))
+})()
 
-var renderEl = document.getElementById('render')
-var sortable = new Sortable(renderEl)
-sortable.handle('.handler')
-sortable.bind('li')
-
+!(function () {
 var data = [{
   index: 1,
   language: 'javascript'
@@ -38,17 +55,14 @@ var data = [{
   index: 6,
   language: 'Rust'
 }]
-!(function () {
 var List = require('mobile-list')
 var tmpl = '<li>{language}</li>'
 var colors = ['tomato', 'gold', 'lightgreen', 'deepskyblue', 'orange', 'violet']
 var list = new List(tmpl, window, {
   selector: '#mobile-list',
   delegate: {
-    remove: tap(function (e, model, el) {
-      //model.remove()
-      console.log(12)
-      swipe.remove()
+    remove: tap(function (e, model) {
+      swipe.clear()
     })
   },
   bindings: {
@@ -65,4 +79,9 @@ list.setData(data)
 var template = '<div class="remove" data-color="index" on-touchstart="remove">删除</div>'
 var swipe = SwipeIt(template)
 swipe.bind(list, 'li')
+// slide up
+swipe.on('remove', function (el) {
+  //el.style.zIndex = 'aoto'
+  el.style[transform] = 'translateX(' + swipe.x + 'px) translateY(-100%)'
+})
 })()
